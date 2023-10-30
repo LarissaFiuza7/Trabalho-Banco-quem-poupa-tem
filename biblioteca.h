04182c59a568a7e09c068aaca793853f6889468c
LarissaFiuza7 <123764342+LarissaFiuza7@users.noreply.github.com>
2023-10-30 03:46:06 +0000

    Adicionar função novo cliente, salvar e cliente e carregar cliente

---
+++ biblioteca.h
@@ -0,0 +1,23 @@
+#ifndef PROJET_1_BIBLIOTECA_H
+#define PROJET_1_BIBLIOTECA_H
+
+// Cria um cadastro de um cliente usando os parâmetros das variáveis nome, cpf, tipo da conta, saldo inicial e a senha
+typedef struct {
+  char nome[100];
+  char CPF[12];
+  char tipo_conta[6];
+  int valor_inicial;
+  int senha;
+} Cliente;
+
+// Cria a lista de clientes
+typedef struct {
+  Cliente clientes[1000];
+  int qtd;
+} ListaDeClientes;
+
+// Variaveis
+void cadastrarCliente(ListaDeClientes *lt);
+void salvarCliente(ListaDeClientes lt, const char *cliente);
+void carregarCliente(ListaDeClientes *lt, const char *cliente);
+#endif // PROJET_1_BIBLIOTECA_H
